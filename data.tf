169f2e325b9452d6de1204149376edd083d4e57c
Hemant Kumar Gupta <106114772+hkg17@users.noreply.github.com>
2026-08-27 20:41:19 +0530

    Add comments to data.tf for clarity
    Added comments to clarify sections for GitHub Actions Runner and existing Key Vault.

--- data.tf
+++ data.tf
@@ -1,3 +1,16 @@
-data "local_file" "cloudinit" {
+###################################################
+# GitHub Actions Runner Cloud-Init
+###################################################
+
+data "local_file" "github_cloudinit" {
   filename = "${path.module}/scripts/cloudinit.yaml"
+}
+
+###################################################
+# Existing GitHub Key Vault
+###################################################
+
+data "azurerm_key_vault" "github" {
+  name                = "kv-github-dev-inc-01"
+  resource_group_name = azurerm_resource_group.github_app.name
 }
